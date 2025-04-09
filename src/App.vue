e3bf125dba5f158534d2be7531024a6839b761a5
Lulu <ldal48848@edu.ucl.dk>
2025-04-09 14:53:51 +0200

    Setup sidebar and topbar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
 <script setup>
 import SideNav from '@/components/SideNav.vue'
+import TopBar from '@/components/TopBar.vue'
 </script>
 
 <template>
     <div class="layout">
       <SideNav />
-      <div class="layout__content">
-        <router-view />
+      <div class="layout__main">
+        <TopBar />
+        <div class="layout__content">
+          <router-view />
+        </div>
       </div>
     </div>
   </template>
 
 <style scoped>
+
+.layout {
+  display: flex;
+  height: 100vh;
+
+  &__main {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    background-color: var(--background-color);
+  }
+
+  &__content {
+    flex: 1;
+    padding: var(--padding);
+    overflow-y: auto;
+  }
+}
 </style>
--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -4,8 +4,14 @@ import "@/assets/main.scss";
 </script>
 
 <template>
-    <div class="sidenav">
-    <router-link to="/" class="sidenav__link">Home</router-link>
-    <router-link to="/calendar" class="sidenav__link">Calendar</router-link>
-  </div>
+   <nav class="sidenav">
+    <router-link to="/" class="sidenav__link">Startside</router-link>
+    <router-link to="/reports" class="sidenav__link">Rapporter & Dokumenter</router-link>
+    <router-link to="/deadlines" class="sidenav__link">Overskredet Deadline</router-link>
+    <router-link to="/users" class="sidenav__link">Brugere & Grupper</router-link>
+    <router-link to="/forms" class="sidenav__link">Skemaer</router-link>
+    <router-link to="/objects" class="sidenav__link">Objekter</router-link>
+    <router-link to="/calendar" class="sidenav__link">Kalender</router-link>
+    <router-link to="/settings" class="sidenav__link">Indstillinger</router-link>
+  </nav>
 </template>
