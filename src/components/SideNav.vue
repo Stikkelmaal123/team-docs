6a3dddf3946a7fd18c99311e2e6f0e9995c4249c
Lulu <ldal48848@edu.ucl.dk>
2025-04-10 11:35:21 +0200

    Icons are added (Not in topbar though yet)

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -4,14 +4,38 @@ import "@/assets/main.scss";
 </script>
 
 <template>
-   <nav class="sidenav">
-    <router-link to="/" class="sidenav__link">Startside</router-link>
-    <router-link to="/reports" class="sidenav__link">Rapporter & Dokumenter</router-link>
-    <router-link to="/deadlines" class="sidenav__link">Overskredet Deadline</router-link>
-    <router-link to="/users" class="sidenav__link">Brugere & Grupper</router-link>
-    <router-link to="/forms" class="sidenav__link">Skemaer</router-link>
-    <router-link to="/objects" class="sidenav__link">Objekter</router-link>
-    <router-link to="/calendar" class="sidenav__link">Kalender</router-link>
-    <router-link to="/settings" class="sidenav__link">Indstillinger</router-link>
+  <nav class="sidenav">
+    <router-link to="/" class="sidenav__link">
+      <img src="@/assets/icons/Overview.png" alt="Startside" class="sidenav__icon" />
+      Startside
+    </router-link>
+    <router-link to="/reports" class="sidenav__link">
+      <img src="@/assets/icons/Folder.png" alt="Rapporter" class="sidenav__icon" />
+      Rapporter & Dokumenter
+    </router-link>
+    <router-link to="/deadlines" class="sidenav__link">
+      <img src="@/assets/icons/Warning.png" alt="Overskredet Deadline" class="sidenav__icon" />
+      Overskredet Deadline
+    </router-link>
+    <router-link to="/users" class="sidenav__link">
+      <img src="@/assets/icons/Groups.png" alt="Brugere & Grupper" class="sidenav__icon" />
+      Brugere & Grupper
+    </router-link>
+    <router-link to="/forms" class="sidenav__link">
+      <img src="@/assets/icons/Schedule.png" alt="Skemaer" class="sidenav__icon" />
+      Skemaer
+    </router-link>
+    <router-link to="/objects" class="sidenav__link">
+      <img src="@/assets/icons/Objects.png" alt="Objekter" class="sidenav__icon" />
+      Objekter
+    </router-link>
+    <router-link to="/calendar" class="sidenav__link">
+      <img src="@/assets/icons/Calendar.png" alt="Kalender" class="sidenav__icon" />
+      Kalender
+    </router-link>
+    <router-link to="/settings" class="sidenav__link">
+      <img src="@/assets/icons/Settings.png" alt="Indstillinger" class="sidenav__icon" />
+      Indstillinger
+    </router-link>
   </nav>
 </template>
