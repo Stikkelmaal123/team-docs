afda8f67805124b86b4c62fb4b05f8138a3d8eee
Mikkel Stal <Mikkel.Staal@outlook.dk>
2025-04-11 10:46:56 +0200

    Firebussy setup

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC5f_6HsBwItpBmwBuhBiHw32Zxoli8qkc",
+  authDomain: "dbi-2025.firebaseapp.com",
+  projectId: "dbi-2025",
+  storageBucket: "dbi-2025.firebasestorage.app",
+  messagingSenderId: "132766601363",
+  appId: "1:132766601363:web:7ff2a996f4366d1ef17d66"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
