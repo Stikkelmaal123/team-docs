29465d77fc1da94a5a4514fdad825c380217cfa4
Lulu <ldal48848@edu.ucl.dk>
2025-04-13 20:10:46 +0200

    A calendar added (its ugly)

--- src/views/CalenderView.vue
+++ src/views/CalenderView.vue
@@ -1,10 +1,15 @@
 <script setup></script>
 
 <template>
-  <div class="calendar-view">
-    <h1>Calendar Page</h1>
-    <CalendarComponent />
+    <h1 class="calendar-page__title">Marts 2025</h1>
+
+<div class="calendar-page__grid">
+  <!-- We'll render calendar days here -->
+  <div class="calendar-page__day" v-for="day in 31" :key="day">
+    <!-- Later fill real date numbers here -->
+    <span class="calendar-page__day-number">{{ day }}</span>
   </div>
+</div>
 </template>
 
 <style lang="scss" scoped></style>
