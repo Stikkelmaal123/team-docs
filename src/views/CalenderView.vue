2e61490f30da2a3b27df5c7a226074037c5d89c4
Lulu <ldal48848@edu.ucl.dk>
2025-04-20 16:00:16 +0200

    more design setup

--- src/views/CalenderView.vue
+++ src/views/CalenderView.vue
@@ -1,13 +1,42 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const daysInMonth = 31
+const startDay = 6
+const totalCells = 42
+
+const calendarDays = Array.from({ length: totalCells }, (_, index) => {
+  const dayNum = index - startDay + 1
+  return {
+    label: dayNum > 0 && dayNum <= daysInMonth ? dayNum : '',
+    hasEvents: dayNum === 14 || dayNum === 19,
+    events: dayNum === 14 ? 21 : dayNum === 19 ? 1 : 0
+  }
+})
+</script>
 
 <template>
+<div class="calendar-page">
     <h1 class="calendar-page__title">Marts 2025</h1>
 
-<div class="calendar-page__grid">
-  <!-- We'll render calendar days here -->
-  <div class="calendar-page__day" v-for="day in 31" :key="day">
-    <!-- Later fill real date numbers here -->
-    <span class="calendar-page__day-number">{{ day }}</span>
+</div>
+<div class="calendar-page__container">
+  <div class="calendar-page__grid">
+    <div
+     class="calendar-page__day"
+      v-for="(day, index) in calendarDays"
+     :key="index"
+      :class="{ 'calendar-page__day--highlighted': day.label === 20 }"
+    >
+      <span class="calendar-page__day-number">{{ day.label }}</span>
+
+     <div
+       v-if="day.hasEvents"
+       class="calendar-page__event"
+      >
+        {{ day.events }} {{ day.events === 1 ? 'Event' : 'Events' }}
+      </div>
+    </div>
   </div>
 </div>
 </template>
